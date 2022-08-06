9dedc300b4b344d52fd0554087a1c5834aa0970b
unknown <berabiswanath338@gmail.com>
2022-08-06 17:49:55 +0530

    first commit

---
+++ src/data/contactsData.js
@@ -0,0 +1,7 @@
+export const contactsData = {
+    email: 'biswanathbera03@gmail.com',
+    phone: '+918637018031',
+    address: 'Chiranjibpur, Haldia, Purba Medinipur - 721657',
+
+    sheetAPI: ''
+}
